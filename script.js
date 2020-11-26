ae08b1111959ef51eae0a3215ffdcf80cde2e7fa
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2020-11-26 22:57:55 +0100

    little changes in js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let accordions = document.querySelectorAll("button.accordion");
 
 for (let i = 0; i < accordions.length; i++) {
-    accordions[i].onclick = function () {
+    accordions[i].addEventListener('click', function () {
 
         this.classList.toggle("active");
         this.nextElementSibling.classList.toggle("show");
-    }
+    })
 }
