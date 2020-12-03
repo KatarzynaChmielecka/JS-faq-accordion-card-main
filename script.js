c4c4068242714ed68368e62b58af7b7a3df47906
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2020-12-03 01:40:49 +0100

    little changes in js

--- script.js
+++ script.js
@@ -1,30 +1,9 @@
-// let accordions = document.querySelectorAll("button.accordion");
-
-// for (let i = 0; i < accordions.length; i++) {
-//     accordions[i].addEventListener('click', function () {
-
-//         this.classList.toggle("active");
-//         this.nextElementSibling.classList.toggle("show");
-//     })
-// }
-
-// let accordions = document.querySelectorAll("button.accordion");
-
-// for (let i = 0; i < accordions.length; i++) {
-//     accordions[i].addEventListener('click', function () {
-
-//         this.classList.toggle("active");
-//         this.nextElementSibling.classList.toggle("active");
-//     })
-// }
-
-
-
 function initAccordion(accordionElem) {
     //when panel is clicked, handlePanelClick is called.          
     function handlePanelClick(event) {
         showPanel(event.currentTarget);
     }
+
     //Hide currentPanel and show new panel.  
     function showPanel(panel) {
         //Hide current one. First time it will be null. 
@@ -33,16 +12,15 @@ function initAccordion(accordionElem) {
         if (expandedPanel) {
             expandedPanel.classList.remove("active");
         }
-        //Show new one
-        // panel.classList.add("active");
-        panel.nextElementSibling.classList.add("active");
 
+        //Show new one
+        panel.nextElementSibling.classList.add("active");
     }
-    var allPanelElems = accordionElem.querySelectorAll(".accordion");
+
+    const allPanelElems = accordionElem.querySelectorAll(".accordion");
     for (let i = 0; i < allPanelElems.length; i++) {
         allPanelElems[i].addEventListener("click", handlePanelClick)
     }
-    //By Default Show first panel //     /* showPanel(allPanelElems[0]) */
 
 }
 initAccordion(document.querySelector(".wrapper-content"));
